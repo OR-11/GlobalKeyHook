73ee6e1679cad43714d9785453e25961ce3a3b0d
OR-11 <93813653+OR-11@users.noreply.github.com>
2024-09-11 01:33:19 +0900

    URLを追加

--- KeyHook.cpp
+++ KeyHook.cpp
@@ -1,4 +1,5 @@
 /*
+* https://tm-b.hatenablog.com/entry/2023/02/23/%E3%80%90C%23%E3%80%91%E5%B7%A6CapsLock%E3%82%92%E7%84%A1%E5%8A%B9%E3%81%AB%E3%81%97%E3%81%A6%E5%B7%A6Ctrl%E3%82%AD%E3%83%BC%E3%81%AB%E5%81%BD%E8%A3%85%E3%81%97%E3%81%A6%E3%81%BF%E3%81%9F
     C#のコードをChatGPTに投げていらないところを消したら動いた！奇跡！
 */
 
